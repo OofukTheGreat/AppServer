03ea3193ab1d199bb114f0c8cd84b9d2f13745ec
User <User@LAB3-3>
2025-01-21 14:23:18 +0200

    added isimage

--- AppServer/Controllers/NRAPIController.cs
+++ AppServer/Controllers/NRAPIController.cs
@@ -117,52 +117,78 @@ namespace AppServer.Controllers
 
         //THis function gets a userId and a profile image file and save the image in the server
         //The function return the full path of the file saved
-        //private async Task<string> SaveProfileImageAsync(int userId, IFormFile file)
-        //{
-        //    //Read all files sent
-        //    long imagesSize = 0;
+        private async Task<string> SaveProfileImageAsync(int userId, IFormFile file)
+        {
+            //Read all files sent
+            long imagesSize = 0;
 
-        //    if (file.Length > 0)
-        //    {
-        //        //Check the file extention!
-        //        string[] allowedExtentions = { ".png", ".jpg" };
-        //        string extention = "";
-        //        if (file.FileName.LastIndexOf(".") > 0)
-        //        {
-        //            extention = file.FileName.Substring(file.FileName.LastIndexOf(".")).ToLower();
-        //        }
-        //        if (!allowedExtentions.Where(e => e == extention).Any())
-        //        {
-        //            //Extention is not supported
-        //            throw new Exception("File sent with non supported extention");
-        //        }
+            if (file.Length > 0)
+            {
+                //Check the file extention!
+                string[] allowedExtentions = { ".png", ".jpg" };
+                string extention = "";
+                if (file.FileName.LastIndexOf(".") > 0)
+                {
+                    extention = file.FileName.Substring(file.FileName.LastIndexOf(".")).ToLower();
+                }
+                if (!allowedExtentions.Where(e => e == extention).Any())
+                {
+                    //Extention is not supported
+                    throw new Exception("File sent with non supported extention");
+                }
 
-        //        //Build path in the web root (better to a specific folder under the web root
-        //        string filePath = $"{this.webHostEnvironment.WebRootPath}\\profileImages\\{userId}{extention}";
+                //Build path in the web root (better to a specific folder under the web root
+                string filePath = $"{this.webHostEnvironment.WebRootPath}\\profileImages\\{userId}{extention}";
 
-        //        using (var stream = System.IO.File.Create(filePath))
-        //        {
-        //            await file.CopyToAsync(stream);
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await file.CopyToAsync(stream);
 
-        //            if (IsImage(stream))
-        //            {
-        //                imagesSize += stream.Length;
-        //            }
-        //            else
-        //            {
-        //                //Delete the file if it is not supported!
-        //                System.IO.File.Delete(filePath);
-        //                throw new Exception("File sent is not an image");
-        //            }
+                    if (IsImage(stream))
+                    {
+                        imagesSize += stream.Length;
+                    }
+                    else
+                    {
+                        //Delete the file if it is not supported!
+                        System.IO.File.Delete(filePath);
+                        throw new Exception("File sent is not an image");
+                    }
 
-        //        }
+                }
 
-        //        return filePath;
+                return filePath;
 
-        //    }
+            }
 
-        //    throw new Exception("File in size 0");
-        //}
+            throw new Exception("File in size 0");
+        }
+        private static bool IsImage(Stream stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+
+            List<string> jpg = new List<string> { "FF", "D8" };
+            List<string> bmp = new List<string> { "42", "4D" };
+            List<string> gif = new List<string> { "47", "49", "46" };
+            List<string> png = new List<string> { "89", "50", "4E", "47", "0D", "0A", "1A", "0A" };
+            List<List<string>> imgTypes = new List<List<string>> { jpg, bmp, gif, png };
+
+            List<string> bytesIterated = new List<string>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                string bit = stream.ReadByte().ToString("X2");
+                bytesIterated.Add(bit);
+
+                bool isImage = imgTypes.Any(img => !img.Except(bytesIterated).Any());
+                if (isImage)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 
 }
